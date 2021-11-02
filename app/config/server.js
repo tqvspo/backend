e690cab8e87fbb9e5a83a55eb9897771181f03e5
tqvspo <trinhquocvan28031991@gmail.com>
2021-11-02 15:46:04 +0700

    init

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a9577d67779a6712dfbe3bd53dfeb96d'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cb4be8a8-aa8a-4789-9b19-fc62d92d58fc'
+};
